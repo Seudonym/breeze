d731f909b4ac95f72427ee325489a0580aa9e977
Seudonym <wk170179@gmail.com>
2026-05-29 17:32:51 +0530

    refactor: split flake into shell.nix

--- flake.nix
+++ flake.nix
@@ -17,38 +17,6 @@
       cudaPackages = pkgs.cudaPackages;
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          clang-tools
-
-          gcc
-          gdb
-          stdenv.cc.cc.lib
-
-          cudaPackages.cudatoolkit
-          cudaPackages.cudnn
-        ];
-
-        shellHook = ''
-          export CUDA_HOME=${cudaPackages.cudatoolkit}
-          export CUDA_PATH=${cudaPackages.cudatoolkit}
-          export PATH=${cudaPackages.cudatoolkit}/bin:$PATH
-          export LD_LIBRARY_PATH=${
-            pkgs.lib.makeLibraryPath [
-              pkgs.stdenv.cc.cc
-              cudaPackages.cudatoolkit
-              cudaPackages.cudnn
-            ]
-          }:/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH
-          export LIBRARY_PATH=${
-            pkgs.lib.makeLibraryPath [
-              cudaPackages.cudatoolkit
-              cudaPackages.cudnn
-            ]
-          }:$LIBRARY_PATH
-          export CPATH=${cudaPackages.cudatoolkit}/include:$CPATH
-        '';
-      };
-
+      devShells.x86_64-linux.default = import ./shell.nix { inherit pkgs cudaPackages; };
     };
 }
---
+++ shell.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  cudaPackages,
+}:
+pkgs.mkShell {
+  packages = with pkgs; [
+    clang-tools
+
+    gcc
+    gdb
+    stdenv.cc.cc.lib
+
+    cudaPackages.cudatoolkit
+    cudaPackages.cudnn
+  ];
+
+  shellHook = ''
+    export CUDA_HOME=${cudaPackages.cudatoolkit}
+    export CUDA_PATH=${cudaPackages.cudatoolkit}
+    export PATH=${cudaPackages.cudatoolkit}/bin:$PATH
+    export LD_LIBRARY_PATH=${
+      pkgs.lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc
+        cudaPackages.cudatoolkit
+        cudaPackages.cudnn
+      ]
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH
+    export LIBRARY_PATH=${
+      pkgs.lib.makeLibraryPath [
+        cudaPackages.cudatoolkit
+        cudaPackages.cudnn
+      ]
+    }:$LIBRARY_PATH
+    export CPATH=${cudaPackages.cudatoolkit}/include:$CPATH
+  '';
+}
